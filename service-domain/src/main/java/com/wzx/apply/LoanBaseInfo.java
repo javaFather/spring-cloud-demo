4037b9643fabd10ddd7a3134279c0a567281b198
wangzx <wangzx@yuminsoft.com>
2018-04-19 18:13:51 +0800

    整合mybatis未完待续

---
+++ service-domain/src/main/java/com/wzx/apply/LoanBaseInfo.java
@@ -0,0 +1,17 @@
+package com.wzx.apply;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+/**
+ * @author YM10174
+ * @date 2018/4/19 16:54:10
+ */
+//@Data
+//@ToString
+public class LoanBaseInfo {
+    private BigInteger loanNo;
+    private String customerCode;
+    private String customerName;
+    private BigDecimal amount;
+}
