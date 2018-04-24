0c008cfcd8327daecc8371b79b5aed51cad54c1d
wangzx <wangzx@yuminsoft.com>
2018-04-24 15:38:36 +0800

    解决models循环依赖

---
+++ common/basic/src/main/java/com/wzx/response/Response.java
@@ -0,0 +1,52 @@
+package com.wzx.response;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @author wangzx
+ * @date 2018/4/24 11:18:03
+ */
+@Data
+public class Response<T> implements Serializable{
+    /**成功码**/
+    public static final String SUCCESS_CODE = "000000";
+
+    /** 响应码**/
+    private String repCode;
+
+    /**响应消息**/
+    private String repMsg;
+
+    /** 操作流水号**/
+    private String flowId;
+
+    /**返回数据**/
+    private T data;
+
+    /**是否成功**/
+    public boolean isSuccess() {
+        return SUCCESS_CODE.equals(this.repCode);
+    }
+
+    public static Response ok(){
+        Response response = new Response();
+        response.repCode = SUCCESS_CODE;
+        return response;
+    }
+
+    public static Response ok(String reqCode,String repMsg){
+        Response response = new Response();
+        response.repCode = reqCode;
+        response.repMsg = repMsg;
+        return response;
+    }
+    public  Response<T> addResponse(T data){
+        this.repCode = SUCCESS_CODE;
+        this.data = data;
+        return this;
+    }
+
+
+}
--- service-api/src/main/java/com/wzx/service/api/provider/HelloEndPoint.java
+++ service-api/src/main/java/com/wzx/service/api/provider/HelloEndPoint.java
@@ -1,17 +1,18 @@
 package com.wzx.service.api.provider;
 
 import com.netflix.appinfo.EurekaInstanceConfig;
-import com.wzx.vo.LoanBaseInfoVO;
+import com.wzx.consumer.entity.LoanBaseInfo;
+import com.wzx.response.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
+
 
 @RestController
 public class HelloEndPoint {
@@ -23,13 +24,13 @@ public class HelloEndPoint {
     private int serverPort = 0;
 
     @RequestMapping(value = "/findByLoanNo", method = RequestMethod.GET)
-    public LoanBaseInfoVO hello() {
+    public Response<LoanBaseInfo> hello() {
         this.logger.info("/hello, instanceId:{}, host:{}", eurekaInstanceConfig.getInstanceId(), eurekaInstanceConfig.getHostName(false));
-            LoanBaseInfoVO loanBaseInfoVO = new LoanBaseInfoVO();
-            loanBaseInfoVO.setLoanNo("20170825BDD3FB");
-            loanBaseInfoVO.setAmount(new BigDecimal(9999.99).setScale(2,BigDecimal.ROUND_HALF_UP));
-            loanBaseInfoVO.setCustomerCode("wangzx");
-            loanBaseInfoVO.setCustomerName("深科技");
-        return loanBaseInfoVO;
+            LoanBaseInfo loanBaseInfo = new LoanBaseInfo();
+            loanBaseInfo.setLoanNo("20170825BDD3FB");
+            loanBaseInfo.setAmount(new BigDecimal(9999.99).setScale(2,BigDecimal.ROUND_HALF_UP));
+            loanBaseInfo.setCustomerCode("wangzx");
+            loanBaseInfo.setCustomerName("深科技");
+        return Response.ok().addResponse(loanBaseInfo);
     }
 }
