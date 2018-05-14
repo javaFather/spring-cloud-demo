e693b85e67ebe95b18d48577b24dac2e92fcf095
wangzx <wangzx@yuminsoft.com>
2018-05-14 11:26:59 +0800

    配置中心

---
+++ server-config/src/main/java/com/wzx/demo/ConfigApplication.java
@@ -0,0 +1,18 @@
+package com.wzx.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author YM10174
+ * @date 2018/5/14 10:52:40
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigApplication {
+        public static void main(String[] args) {
+            SpringApplication.run(ConfigApplication.class, args);
+        }
+
+}
