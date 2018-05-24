541be122e2ead04c8e3574c6c3bb6d14a7a262e1
wangzx <wangzx@yuminsoft.com>
2018-05-24 17:33:10 +0800

    mq消费者

--- common/rocketmq/src/main/java/com/wzx/demo/rocketmq/common/AbstractMQConsumer.java
+++ common/rocketmq/src/main/java/com/wzx/demo/rocketmq/common/AbstractMQConsumer.java
@@ -3,12 +3,19 @@ package com.wzx.demo.rocketmq.common;
 import com.wzx.demo.consumerEnum.RocketMqEnum;
 import lombok.Data;
 import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListener;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.client.exception.MQClientException;
 import org.apache.rocketmq.common.consumer.ConsumeFromWhere;
 import org.apache.rocketmq.common.message.MessageExt;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.List;
 import java.util.Set;
+import java.util.UUID;
 
 /**
  * @author YM10174
@@ -25,6 +32,14 @@ public abstract class AbstractMQConsumer {
     private ConsumeFromWhere consumeFromWhere = ConsumeFromWhere.CONSUME_FROM_FIRST_OFFSET;
     private boolean isStarted;
     private int delayLevelWhenNextConsume = 0;
+
+    private String topics;
+
+    private String nameServer;
+
+    private MessageListener listener;
+
+    public AbstractMQConsumer(){}
 
     public AbstractMQConsumer(String consumerGroup,String nameAddr){
         this.consumerGroup = consumerGroup;
@@ -43,6 +58,54 @@ public abstract class AbstractMQConsumer {
      * @return
      */
     public abstract boolean consumerMsg(MessageExt msgExt);
+
+    /**
+     *  MQ的消费类
+     * @param
+     * @author wangzx
+     * @date 2018/5/23 15:28
+     */
+
+    public void init() {
+        mqPushConsumer = new DefaultMQPushConsumer(consumerGroup);
+        mqPushConsumer.setNamesrvAddr(nameServer);
+        try {
+            mqPushConsumer.subscribe(topics, "*");
+        } catch (MQClientException e) {
+            e.printStackTrace();
+        }
+        mqPushConsumer.setInstanceName(UUID.randomUUID().toString());
+        mqPushConsumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_LAST_OFFSET);
+        mqPushConsumer.registerMessageListener((MessageListenerConcurrently) this.listener);
+
+        try {
+            mqPushConsumer.start();
+        } catch (MQClientException e) {
+            e.printStackTrace();
+        }
+        System.out.println("消息队列启动! group=" + mqPushConsumer.getConsumerGroup() + " instance=" + mqPushConsumer.getInstanceName()
+        );
+    }
+
+      /**
+       *  MQ消息的监听接口类
+       * @param
+       * @author wangzx
+       * @date 2018/5/23 15:28
+       */
+        public class RocketMQListener implements MessageListenerConcurrently {
+        @Override
+            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs, ConsumeConcurrentlyContext context) {
+//        System.out.println("get data from rocketMQ:" + msgs);
+                for (MessageExt message : msgs) {
+
+                    String msg = new String(message.getBody());
+                    System.out.println("msg data from rocketMQ:" + msg);
+                }
+
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+            }
+        }
 
 
 
